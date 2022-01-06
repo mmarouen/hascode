593618bc156eccc3f4aec89060d2932c141d85ca
mmarouen <azzouz.marouen@gmail.com>
2022-01-06 15:32:57 +0100

    adding parser, script runs for all files

--- more_pizza/more_pizza_ortools.py
+++ more_pizza/more_pizza_ortools.py
@@ -1,20 +1,26 @@
 import numpy as np
 from ortools.sat.python import cp_model
 
-# a_example
-n_slices = 17
-pizza = [2, 5, 6, 8]
-# b_example
-n_slices = 100
-pizza = [4, 14, 15, 18, 29, 32, 36, 82, 95, 95]
-# c_medium
-n_slices = 4500
-pizza = [7, 12, 12, 13, 14, 28, 29, 29, 30, 32, 32, 34, 41, 45, 46,
-         56, 61, 61, 62, 63, 65, 68, 76, 77, 77, 92, 93, 94, 97, 103, 113, 114, 114,
-         120, 135, 145, 145, 149, 156, 157, 160, 169, 172, 179, 184, 185, 189, 194, 195, 195]
-# a parser should be added to sread the files
-
+filenames = ['a_example.in', 'b_small.in', 'c_medium.in', 'd_quite_big.in', 'e_also_big.in']
+index = 4
+file_url = 'more_pizza/data/' + filenames[index]
+# parse input
+file = open(file_url, 'r')
+lines = file.readlines()
+for l, line in enumerate(lines):
+    if(l == 0):
+        n_slices = int(line.split(' ')[0])
+    if(l == 1):
+        line = line.rstrip('\r\n')
+        pizza = [int(p) for p in line.split(' ')]
+file.close()
 n_pizzas = len(pizza)
+
+print(f'Problem setup for file {filenames[index]}:')
+print(f'Maxmimum slices to order {n_slices}, out of {n_pizzas} variants')
+print(f'Pizza slices count per type: {pizza}')
+print('----------------------------')
+
 model = cp_model.CpModel()
 
 """
@@ -39,11 +45,13 @@ solver = cp_model.CpSolver()
 status = solver.Solve(model)
 
 if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
-    print(f'Solution found: total score {solver.Value(total_score)}')
+    n_ordered_pizzas = 0
     output = 'selected pizzas models: '
     for m in range(n_pizzas):
         if(solver.Value(x[m])):
             output += f'{m} '
+            n_ordered_pizzas += 1
+    print(f'Solution found:\nTotal pizza slices {solver.Value(total_score)} from a total of {n_ordered_pizzas} pizzas.\nTarget is {n_slices} slices')
     print(output)
 else:
     print('No solution found.')
